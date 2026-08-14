163555c5d91208d34773a588d1f8d729c064d16d
Abhishek Yadav <abhishek.recsonbhadra@gmail.com>
2026-08-14 21:17:59 +0530

    Sqrt(x)

---
+++ 69_Sqrt(x).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0)return 0;
+        long long s = 0;
+        long long e = x;
+        int ans = 0;
+        while(s<=e){
+            long long mid = s +(e-s)/2;
+            if(mid*mid <= x){
+                ans = mid;
+                s = mid+1;
+            }
+            else{
+                e = mid -1;
+            }
+        }
+        return ans;
+        
+    }
+};
